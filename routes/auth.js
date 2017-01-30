ab1ba30d8f69e0bd38e514462269aa48e530b9cd
val rigodon <rvrigodon@gmail.com>
2017-01-29 22:14:52 -0500

    add auth routes

--- routes/auth.js
+++ routes/auth.js
@@ -17,3 +17,21 @@ router.post('/register', (req, res, next) => {
 });
 //This code ^  will send all the information from the createuser files to the database
 //created with sequelize
+router.get('/login', authHelpers.loginRedirect, (req, res) => {
+    res.render('auth/login');
+});
+//This adds a login route
+router.post('/login', passport.authenticate('local', {
+    successRedirect: '/user',
+    failureRedirect: '/auth/login',
+    failureFlash: true
+}));
+//This checks to see if the user already has an account and if they are logged in
+//So that they won't be led to a 'registration' page
+router.get('/logout', (req, res) => {
+    req.logout();
+    res.redirect('/');
+});
+
+module.exports = router;
+//This line of code ^ is meant for logging out the user and killing the session
